faf8a4267fac4218fd780209179f05aab14bf99f
Matheus Angelicio <matheus.angelicio00@gmail.com>
2023-03-27 10:31:34 -0300

    adicionar um contador ao exercicio

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ class App extends Component{
     super(props);
 
     this.state = {
-      titulo: "Primeira página com Class Component"
+      titulo: "Primeira página com Class Component",
+      contador: 0
     }
 
     this.mudar = this.mudar.bind(this);
+    this.aumentar = this.aumentar.bind(this);
   }
 
   mudar(){
@@ -18,11 +20,21 @@ class App extends Component{
     this.setState({titulo: novoTitulo});
   }
 
+  aumentar() {
+    let state = this.state;
+    state.contador += 1;
+    this.setState(state)
+  }
+
   render(){
     return (
       <div>
         <h1> {this.state.titulo} </h1>
         <button onClick={this.mudar}>Mudar Título</button>
+        <h1></h1>
+        <h1></h1>
+        <button onClick={this.aumentar}>+</button>
+        {this.state.contador}
         
       </div>
     )
